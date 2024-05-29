1afb2d3ab19d6f2821a978762f7bce61fe00828f
Michal Massachi <michal2792000@gmail.com>
2024-05-29 14:27:02 +0300

    updating port

--- src/server.js
+++ src/server.js
@@ -19,12 +19,13 @@ if (process.env.NODE_ENV !== 'production') {
   server.listen(process.env.PORT);
 } else {
   console.log('PRODUCTION');
-  const options2 = {
-    key: fs.readFileSync('../client-key.pem'),
-    cert: fs.readFileSync('../client-cert.pem'),
-  };
-  server = https.createServer(options2, app);
-  server.listen(process.env.HTTPS_PORT);
+  // const options2 = {
+  //   key: fs.readFileSync('../client-key.pem'),
+  //   cert: fs.readFileSync('../client-cert.pem'),
+  // };
+  // server = https.createServer(options2, app);
+  // server.listen(process.env.HTTPS_PORT);
+  server.listen(process.env.PORT);
 }
 
 
